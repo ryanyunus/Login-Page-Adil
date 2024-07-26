ad03655d2e02813b5b05159d62c273ad35db19fe
ryanyunus <ryandinus@gmail.com>
2024-07-26 10:19:56 +0700

    revisi pak ryan

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-const wrapper = document.querySelector('.wrapper');
-const loginlink = document.querySelector('.login-link');
-const registerlink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnlogin-popup');
-const iconclose = document.querySelector('.icon-close');
+const loginlink = document.querySelector(".login-link");
+const registerlink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnlogin-popup");
+const iconclose = document.querySelector(".icon-close");
+const wrapper = document.querySelector(".wrapper");
 
+if (registerlink) {
+  registerlink.addEventListener("click", () => {
+    wrapper.classList.add("active");
+  });
+}
 
-registerlink.addEventListener('click', ()=>{
-    wrapper.classList.add('active');
-})
+if (loginlink) {
+  loginlink.addEventListener("click", () => {
+    wrapper.classList.remove("active");
+  });
+}
 
-loginlink.addEventListener('click', ()=>{
-    wrapper.classList.remove('active');
-})
+if (btnPopup) {
+  btnPopup.addEventListener("click", () => {
+    wrapper.classList.add("active-popup");
+  });
+}
 
-btnPopup.addEventListener('click', ()=>{
-    wrapper.classList.add('active-popup');
-})
-
-iconclose.addEventListener('click', ()=>{
-    wrapper.classList.remove('active-popup');
-})
-
+if (iconclose) {
+  iconclose.addEventListener("click", () => {
+    wrapper.classList.remove("active-popup");
+  });
+}
